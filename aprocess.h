f807e7e90cc8bd03241daa533011efbf44ca048e
Dongfeng Gu <gudongfeng@outlook.com>
2016-05-12 13:04:20 -0400

    add the parse function for xml file

--- aprocess.h
+++ aprocess.h
@@ -625,6 +625,54 @@ public:
         file.close();
     }
 
+	static void parseXMLAnnotations(const string &filename, vector<vector<vector<Point2f>>> &_data)
+	{		
+		string input_xml;
+		std::string line;
+		std::ifstream in(filename);
+		// read the file into input_XML
+		while (getline(in, line))
+			input_xml += line;
+
+		vector<char> xml_copy(input_xml.begin(), input_xml.end());
+		xml_copy.push_back('\0');
+
+		xml_document<> doc;
+		doc.parse<parse_declaration_node | parse_no_data_nodes>(&xml_copy[0]);
+
+		long lcount = 0;
+		_data.clear();
+		
+		// frame loop
+		for (xml_node<> *frame_node = doc.first_node(); frame_node; frame_node = frame_node->next_sibling())
+		{
+			vector<vector<Point2f>> _line;
+			_data.push_back(_line);
+			// box loop
+			for (xml_node<> *box_node = frame_node->first_node(); box_node; box_node = box_node->next_sibling())
+			{
+				vector<Point2f> pts;
+				for (xml_attribute<> *point = box_node->first_attribute(); point; point = point->next_attribute())
+				{
+					if (strcmp(point->name(), "boxNumber") == 0) { continue; }
+					char *value = point->value();
+					// parse the value
+					char value_array[50];
+					strncpy(value_array, value, sizeof(value_array));
+					char *single_axis = strtok(value_array, ",");
+					std::vector<char*> xy;
+					while (NULL != single_axis)
+					{
+						xy.push_back(single_axis);
+						single_axis = strtok(NULL, ",");
+					}
+					pts.push_back(Point2f(atof(xy[0]), atof(xy[1])));					
+				}
+				_data[lcount].push_back(pts);
+			}
+			lcount++;
+		}
+	}
 };
 
 
